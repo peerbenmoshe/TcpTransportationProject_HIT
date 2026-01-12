6cb5fd4461c621db894e4c1549f99ebf01e3d470
peerbenmoshe <peer2112@gmail.com>
2026-01-12 22:46:37 +0200

    multi client server pretty done

--- clientApp.py
+++ clientApp.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 from serverApp import HOST, PORT
 
 def main():
@@ -7,25 +8,62 @@ def main():
 def start_client():
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        clientSocket.connect((HOST, PORT))
-        print(f"[CLIENT] Connected to the server: {HOST}:{PORT}")
-        welcomeMessage = clientSocket.recv(1024).decode("utf-8")
-        print(f"[CLIENT] [SERVER RESPONSE] {welcomeMessage}")
+        name = login(clientSocket)
+        stopEvent = threading.Event()
+        listenerThread = threading.Thread(target=listen_for_messages, args=(clientSocket, name, stopEvent), daemon=True)
+        listenerThread.start()
 
         while True:
-            message = input("[CLIENT] Enter message to send (or 'exit' to quit): ")
-            if message.lower() == "exit":
-                print("[CLIENT] Exiting...")
+            message = input("[CLIENT] Enter a message: ")
+            if message.lower() == "/exit":
+                print("[CLIENT] Exiting...")   
                 break
             clientSocket.sendall(message.encode("utf-8"))
-            response = clientSocket.recv(1024).decode("utf-8")
-            print(f"[CLIENT] [SERVER RESPONSE] {response}")
-
 
     except ConnectionRefusedError:
         print(f"[CLIENT] Could not connect to the server: {HOST}:{PORT}")
+    except Exception as e:
+        print(f"[CLIENT] An error occurred: {e}")
     finally:
         clientSocket.close()
+        stopEvent.set()
+        listenerThread.join()
+        
+
+
+def login(clientSocket):
+    clientSocket.connect((HOST, PORT))
+    print(f"[CLIENT] Connected to the server: {HOST}:{PORT}")
+    serverPrompt = clientSocket.recv(1024).decode("utf-8")
+    name = input(f"[CLIENT] {serverPrompt} ")
+    while True:
+        clientSocket.sendall(name.encode("utf-8"))
+        response = clientSocket.recv(1024).decode("utf-8")
+        if "Name already taken" not in response:
+            break
+        name = input(f"[CLIENT] {response}")
+    print(f"[CLIENT] {response}")
+    return name
+
+
+def listen_for_messages(clientSocket, name, stopEvent):
+    while not stopEvent.is_set():
+        try:
+            message = clientSocket.recv(1024).decode("utf-8")
+            if message:
+                if "[SERVER]" in message:
+                    print(f"\n{message}\n", end="")
+                else:
+                    print(f"\n{message}\n[CLIENT] Enter a message: ", end="")
+            else:
+                break
+        except ConnectionAbortedError:
+            break
+        except Exception as e:
+            print(f"[CLIENT] Error receiving message from server: {e}")
+            break
+        stopEvent.wait(0.1)
+
 
 if __name__ == "__main__":
     main()
--- serverApp.py
+++ serverApp.py
@@ -6,6 +6,8 @@ HOST = helperApp.get_host_ip()
 PORT = 12345
 
 def main():
+    global connectedClients
+    connectedClients = {}
     start_server()
 
 def start_server():
@@ -13,37 +15,117 @@ def start_server():
     serverSocket.bind((HOST, PORT))
     serverSocket.listen()
     print(f"[SERVER] Listening on {HOST}:{PORT}")
-
+    
     while True:
         clientSocket, addr = serverSocket.accept()
         print(f"[SERVER] Established connection with {addr}")
         clientThread = threading.Thread(target=handle_client, args=(clientSocket, addr))
         clientThread.start()
-        print(f"[SERVER] Active connections: {threading.active_count() - 1}")
+        print(f"[SERVER] Active connections: {len(connectedClients) + 1}")
 
 
 def handle_client(clientSocket, addr):
-    print(f"[SERVER] Connected to {addr}")
     try:
-        welcomeMessage = "You are connected to the server!"
-        clientSocket.sendall(welcomeMessage.encode("utf-8"))
+        name = ask_client_name(clientSocket)
+        connectedClients[name] = clientSocket
+        print(f"[SERVER] {name} : {addr} has joined the chat")
+        welcomeMessage = f"Welcome to the chat, {name}!\n"
+        showCommands = """\tShow active clients: /show
+        Send to specific client: /msg <client_name> <message>
+        Send to all clients: /all <message>
+        Show these commands: /help
+        Exit chat: /exit"""
+        clientSocket.sendall((welcomeMessage+showCommands).encode("utf-8"))
         
         while True:
             data = clientSocket.recv(1024)
             if not data:
                 break
-            print(f"[SERVER] Received from {addr}: {data.decode("utf-8")}")
-            serverResponse = f"I received: {data.decode("utf-8").upper()}"
-            clientSocket.sendall(serverResponse.encode("utf-8"))  # Echo back the received data
+            process_data(data.decode("utf-8"), clientSocket, name)
+
     except ConnectionResetError:
         print(f"[SERVER] Connection with client: {addr} was reset")
     finally:
         clientSocket.close()
-        print(f"[SERVER] Connection with client: {addr} closed")
+        del connectedClients[name]
+        print(f"[SERVER] {name} : {addr} has left the chat, connection closed")
+        print(f"[SERVER] Active connections: {len(connectedClients)}")
 
+
+def ask_client_name(clientSocket):
+    askName = "What's your name?"
+    while True:
+        clientSocket.sendall(askName.encode("utf-8"))
+        name = clientSocket.recv(1024).decode("utf-8")
+        if name not in connectedClients.keys():
+            return name
+        askName = "Name already taken. Please provide another name: "
+
+
+def process_data(data, clientSocket, senderName):
+    parts = data.strip().split(' ', 1)
+    if not parts:
+        return None
+    command = parts[0]
+    if command == "/show":
+        show_active_clients(clientSocket)
+    elif command == "/help":
+        show_help(clientSocket)
+    elif command == "/all":
+        broadcast_message(parts, clientSocket, senderName)
+    elif command == "/msg":
+        direct_message(parts, clientSocket, senderName)
+    else:
+        clientSocket.sendall("[SERVER] Unknown command. Type /help for a list of commands.".encode("utf-8"))
 
 
     
+
+def direct_message(parts, clientSocket, senderName):
+    if len(parts) < 2:
+        clientSocket.sendall("[SERVER] Usage: /msg <client_name> <message>".encode("utf-8"))
+        return
+    
+    destinationName, message = parts[1].split(' ', 1)
+    if destinationName in connectedClients:
+        if message.strip() == "":
+            clientSocket.sendall("[SERVER] Message cannot be empty.".encode("utf-8"))
+            return
+        destinationSocket = connectedClients[destinationName]
+        destinationSocket.sendall(f"[DM from {senderName}] {message}".encode("utf-8"))
+    else:
+        clientSocket.sendall(f"[SERVER] Client {destinationName} is not found. Use /show to see active clients.".encode("utf-8"))
+
+
+def broadcast_message(parts, clientSocket, senderName):
+    if len(parts) < 2:
+        clientSocket.sendall("[SERVER] Usage: /all <message>".encode("utf-8"))
+        return
+    
+    message = parts[1]
+    if message.strip() == "":
+        clientSocket.sendall("[SERVER] Message cannot be empty.".encode("utf-8"))
+        return
+    for destinationSocket in connectedClients.values():
+        if destinationSocket != clientSocket:
+            destinationSocket.sendall(f"[Broadcast from {senderName}] {message}".encode("utf-8"))
+
+
+def show_active_clients(clientSocket):
+    activeClients = ", ".join(connectedClients.keys())
+    response = f"[SERVER] Active clients: [{activeClients}]"
+    clientSocket.sendall(response.encode("utf-8"))
+
+
+def show_help(clientSocket):
+    helpMessage = """[SERVER] Available commands:
+    Show active clients: /show
+    Send to specific client: /msg <client_name> <message>
+    Send to all clients: /all <message>
+    Show these commands: /help
+    Exit chat: /exit"""
+    clientSocket.sendall(helpMessage.encode("utf-8"))
+        
 
 if __name__ == "__main__":
     main()
